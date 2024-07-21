a3f0b60a9fad1ff5c40f33b1b38116b21ffcf7e3
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2024-07-21 15:04:50 -0400

    newEndpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('nuevo')
+  newEndpoint() {
+    return 'Yo soy un nuevo Endpoint';
+  }
 }
