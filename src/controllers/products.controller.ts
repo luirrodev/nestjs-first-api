97b14d2ba1c73ef10d6cad2d3b4de1aef92a1ec2
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2024-07-21 17:01:13 -0400

    finish class

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,25 +1,39 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
   @Get(':productID')
-  getProduct(@Param() params: any) {
-    return `product id => ${params.productID}`;
-  }
-
-  @Get()
-  getProducts(
-    @Query('limit') limit = 100,
-    @Query('offset') offset = 10,
-    @Query('brand') brand: string,
-  ) {
-    return `products(limit)  => ${limit} 
-            products(offset) => ${offset} 
-            products(brand)  => ${brand}`;
+  get(@Param() params: any) {
+    return {
+      message: `product id => ${params.productID}`,
+    };
   }
 
   @Get('products_2/:productID')
   getProduct_2(@Param('productID') productID: number) {
-    return `product id => ${productID}`;
+    return {
+      message: `product id => ${productID}`,
+    };
+  }
+
+  @Get()
+  getAll(
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 10,
+    @Query('brand') brand: string,
+  ) {
+    return {
+      message: `products(limit)  => ${limit} 
+                products(offset) => ${offset} 
+                products(brand)  => ${brand}`,
+    };
+  }
+
+  @Post()
+  create(@Body() payload: any) {
+    return {
+      message: 'accion para crear',
+      payload,
+    };
   }
 }
