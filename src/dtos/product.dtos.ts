fbc3069b08667bedc61fdca78bdd037ab2ff31ec
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2024-08-07 23:20:43 -0400

    finish class

--- src/dtos/product.dtos.ts
+++ src/dtos/product.dtos.ts
@@ -1,15 +1,35 @@
-export interface CreateProductDTO {
+import {
+  IsString,
+  IsNumber,
+  IsUrl,
+  IsNotEmpty,
+  IsPositive,
+} from 'class-validator';
+
+import { PartialType } from '@nestjs/mapped-types';
+
+export class CreateProductDTO {
+  @IsString()
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly description: string;
-  readonly price: number;
-  readonly stock: number;
+
+  @IsUrl()
+  @IsNotEmpty()
   readonly image: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
+  readonly price: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
+  readonly stock: number;
 }
 
-export interface UpdateProductDTO {
-  readonly name?: string;
-  readonly description?: string;
-  readonly price?: number;
-  readonly stock?: number;
-  readonly image?: string;
-}
+export class UpdateProductDTO extends PartialType(CreateProductDTO) {}
