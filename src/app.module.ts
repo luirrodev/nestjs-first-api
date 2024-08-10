e83eb04b5af4d44e284555ce0580bd0fe39a1253
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2024-08-09 21:08:20 -0400

    Tipado de Variables de Ambientes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import config from './config';
 
 // Enviroments
 import { enviroments } from './enviroments';
@@ -14,6 +15,7 @@ import { ProductsModule } from './products/products.module';
   imports: [
     ConfigModule.forRoot({
       envFilePath: enviroments[process.env.NODE_ENV] || '.env',
+      load: [config],
       isGlobal: true,
     }),
     UsersModule,
