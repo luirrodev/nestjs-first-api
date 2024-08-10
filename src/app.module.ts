0dbdfc3139cba8a9d48b04316280f97dbbd8a74e
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2024-08-09 21:23:02 -0400

    validacion de env. y cofig

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,13 @@
-import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { Module } from '@nestjs/common';
+import * as Joi from 'joi';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import config from './config';
-
-// Enviroments
 import { enviroments } from './enviroments';
-
-// Modules
-import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
+import config from './config';
 
 @Module({
   imports: [
@@ -17,6 +15,11 @@ import { ProductsModule } from './products/products.module';
       envFilePath: enviroments[process.env.NODE_ENV] || '.env',
       load: [config],
       isGlobal: true,
+      validationSchema: Joi.object({
+        API_KEY: Joi.number().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+      }),
     }),
     UsersModule,
     ProductsModule,
