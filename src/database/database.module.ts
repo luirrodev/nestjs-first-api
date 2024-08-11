79d1bfdc1b080d57462a0e35bdacc9edc51f9fc0
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2024-08-10 20:05:47 -0400

    agregando variables de entorno

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,15 +1,9 @@
 import { Global, Module } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
 import { Client } from 'pg';
 
-const client = new Client({
-  user: 'root',
-  host: 'localhost',
-  database: 'my_db',
-  password: '12345678',
-  port: 5432,
-});
+import config from 'src/config';
 
-client.connect();
 // client.query('SELECT * FROM public.task', (err, res) => {
 //   console.error(err);
 //   console.log(res.rows);
@@ -20,7 +14,22 @@ client.connect();
   providers: [
     {
       provide: 'PG_CLIENT',
-      useValue: client,
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { user, host, database, password, port } = configService.postgres;
+
+        const client = new Client({
+          user,
+          host,
+          database,
+          password,
+          port,
+        });
+
+        client.connect();
+
+        return client;
+      },
+      inject: [config.KEY],
     },
   ],
   exports: ['PG_CLIENT'],
