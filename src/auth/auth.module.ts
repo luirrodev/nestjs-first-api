41cd6391246ce73b2a43416bd91af11399e19488
Luis Ramon Diaz <diazluisramon.dev@gmail.com>
2024-09-01 14:01:14 -0400

    implementando locacl-strategy con el controlador

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,11 @@ import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './services/auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { LocalStrategyService } from './strategies/local-strategy.service';
+import { AuthController } from './controllers/auth.controller';
 
 @Module({
   imports: [UsersModule, PassportModule],
   providers: [AuthService, LocalStrategyService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
--- src/auth/strategies/local-strategy.service.ts
+++ src/auth/strategies/local-strategy.service.ts
@@ -8,13 +8,15 @@ import { AuthService } from '../services/auth.service';
 @Injectable()
 export class LocalStrategyService extends PassportStrategy(Strategy, 'local') {
   constructor(private authService: AuthService) {
-    super();
+    super({
+      usernameField: 'email',
+    });
   }
 
   async validate(email: string, password: string) {
-    const user = this.authService.validateUser(email, password);
+    const user = await this.authService.validateUser(email, password);
     if (!user) {
-      throw new UnauthorizedException('Not allow');
+      throw new UnauthorizedException('Wrong password');
     }
     return user;
   }
